aac6d7a58b7c6ddb8917645abb97365f1ebcc83b
kathfree3 <kfree3@seas.upenn.edu>
2022-02-04 11:55:20 -0500

    idek

---
+++ wordle/src/App.js
@@ -0,0 +1,13 @@
+
+import Header from "./components/Header"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <h1> hi </h1>
+    </>
+  )
+}
+
+export default App
---
+++ wordle/src/components/Header.js
@@ -0,0 +1,12 @@
+
+import s from 'styled-components'
+
+const Header = () => {
+  return (
+    <>
+      <h1> WORDLE </h1>
+    </>
+  )
+}
+
+export default Header
